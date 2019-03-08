008827ac8ce9234fc80168fc0880e3d70946e160
zen <zennyosh@gmail.com>
2019-03-08 14:44:46 +0900

    add OAuth2 auth

--- ops/sample.js
+++ ops/sample.js
@@ -1,24 +1,61 @@
-console.log("Hello World!");
+$(function(){
+  console.log("Hello World!");
+});
 
 /**
  * https://github.com/fvdm/nodejs-epo-ops/blob/develop/epo-ops.js
  *
  */
+token = "";
+consumer_key = "mavrkKzZhP0dMNNbXnrAzkCmkywUac2f";
+consumer_secret = "2klF2fBMMsDYqdf2";
+key = consumer_key + ":" + consumer_secret;
+console.log("key = " + key);
+base64 = window.btoa(key);
+console.log(base64);
 
 $(function(){
-  consumer_key = "mavrkKzZhP0dMNNbXnrAzkCmkywUac2f";
-  consumer_secret = "2klF2fBMMsDYqdf2";
+  $('#btn').on('click', function() {
+    $('#txt').html("認証中");
 
-  key = consumer_key + ":" + consumer_secret;
-  console.log("key = " + key);
+    if (token) {
+      $('#txt').html("認証済");
+      console.log(token);
+    } else {
 
-  base64 = window.btoa(key);
-//base64 = Buffer.from (key) .toString ('base64');
+    $.ajax({
+      url: "https://ops.epo.org/3.2/auth/accesstoken",
+      type: "POST",
+      headers: {
+        "Authorization" : "Basic " + base64,
+        "Content-Type" : "application/x-www-form-urlencoded"
+      },
+      data: "grant_type=client_credentials",
+      dataType : 'json',
+    }).done(function(data) {
+      console.log(data.access_token);
+//      console.log(data);
+      if (data.access_token) {
+        token = data.access_token;
+      } else {
+        alert('該当するデータが見つかりませんでした');
+      }
+    }).fail(function(data) {
+      console.log(data);
+      if (data.status = "401") {
+        alert('認証に失敗しました:' + data.status);
+      } else {
+        alert('通信に失敗しました:' + data.status);
+      }
+    }).always(function(data) {
+      // 完了時
+    });
 
-  console.log(base64);
+    $('#txt').html("key = " + key + "<br>" + "enc = " + base64);
+  }
+  });
 });
 
 $(function(){
-  $('#txt').html("key = " + key + "<br>" + "enc = " + base64);
   $('#txt').css('color', '#0000FF');
 });
